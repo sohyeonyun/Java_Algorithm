21890b090e491f6974515e98c4ea7bcd8f2a572f
윤소현 <wja06018@naver.com>
2022-09-08 18:34:27 +0900

    20220904

--- 20220904/src/BOJ_1167.java
+++ 20220904/src/BOJ_1167.java
@@ -7,12 +7,25 @@ import java.util.StringTokenizer;
 /*
  * 트리의 지름
  */
+
+//총 dfs를 두 번 시행하면 된다.
+// step 1) 아무 정점 기준으로 dfs를 한다.
+// 한 번 dfs를 하면, 지름의 양 끝점 중 하나를 찾을 수 있다.
+// 왜? 어떤 정점을 기준으로 하더라도 거리가 최대일 때가 지름의 양 끝점 중 하나다.
+// step 2) 찾은 끝점 기준으로  dfs를 한다.
+// 이 때의 거리가 가장 길 때가 트리의 지름이다.
+/*
+ * 노드를 구슬, 간선을 실이라고 생각한다면, 
+ * 구슬 하나를 잡았을 때 축 늘어지는 나머지 구슬 중 가장 실이 길게 늘어나는게 지름의 끝점
+ * 그 끝 구슬을 다시 잡고 늘어뜨리면? 가장 긴 실이 무조건 최장 길이가 된다.
+ */
 public class BOJ_1167 {
 
 	static int V;
 	static ArrayList<Node>[] list;
 	static boolean[] v;
 	static long MAX = 0;
+	static int endNode = 0;
 
 	public static void main(String[] args) throws IOException {
 
@@ -42,14 +55,14 @@ public class BOJ_1167 {
 			}
 		}
 
-		// 한 정점씩 dfs로 거리 계산
-		for (int i = 1; i <= V; i++) {
-			// 방문 체크 배열
-			v = new boolean[V + 1];
-			v[i] = true;
-			// 
-			dfs(i, 0);
-		}
+		// 1번 기준으로 시작
+		v = new boolean[V + 1];
+		v[1] = true;
+		dfs(1, 0);
+		// 찾은 끝점을 기준으로 시작
+		v = new boolean[V + 1];
+		v[endNode] = true;
+		dfs(endNode, 0);
 		
 		System.out.println(MAX);
 
@@ -57,14 +70,18 @@ public class BOJ_1167 {
 
 	static void dfs(int idx, long sum) {
 
-		for(Node node: list[idx]) {
+		for (Node node : list[idx]) {
 			int cur = node.to;
-			if(v[cur]) continue;
+			if (v[cur])
+				continue;
 			v[cur] = true;
 			dfs(cur, sum + node.w);
 		}
 		
-		MAX = Math.max(MAX, sum);
+		if(MAX < sum) {
+			MAX = sum;
+			endNode = idx;
+		}
 	}
 
 	static class Node {
