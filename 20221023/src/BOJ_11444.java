b42c44aa5222564d52405897512f6cfc8481a031
윤소현 <wja06018@naver.com>
2022-10-31 22:59:40 +0900

    20221031

--- 20221023/src/BOJ_11444.java
+++ 20221023/src/BOJ_11444.java
@@ -1,24 +1,68 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /*
- * 피보나치 수 6 - ???
+ * 피보나치 수 6 - 행렬
  */
 public class BOJ_11444 {
 
-	public static void main(String[] args) {
+	final static long MOD = 1000000007;
+	public static long[][] origin = { { 1, 1 }, { 1, 0 } }; // 초기값을 갖고있는 행렬
 
-		Scanner sc = new Scanner(System.in);
-		long N = sc.nextLong();
-		
-		long a = 0;
-		long b = 1; 
-		long c = 1;
-		for(int i=2; i<=N; i++) {
-			c = (a + b) % 1_000_000_007;
-			a = b;
-			b = c;
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		/*
+		 *                n
+		 *       | 1   1 |    | F(n+1)  F(n)  |
+		 * A^n = |       |  = |               |
+		 *       | 1   0 |    |  F(n)  F(n-1) |
+		 */
+
+		long[][] A = { { 1, 1 }, { 1, 0 } };
+
+		long N = Long.parseLong(br.readLine());
+
+		// Fn 을 구하려면 A행렬을 n-1제곱 한 뒤 반환된 행렬 A11 원소를 출력하면 된다.
+
+		System.out.println(pow(A, N - 1)[0][0]);
+	}
+
+	// 행렬 제곱 분할정복 메소드
+	public static long[][] pow(long[][] A, long exp) {
+
+		// 지수가 1 또는 0일 땐 A를 return한다.
+		if (exp == 1 || exp == 0) {
+			return A;
 		}
-		System.out.println(c);
+
+		// 지수를 절반으로 분할하여 재귀호출
+		long[][] ret = pow(A, exp / 2);
+
+		// 하위 재귀에서 얻은 행렬을 제곱해준다.
+		ret = multiply(ret, ret);
+
+		// 만약 지수가 홀수라면 마지막에 A^1 (origin)을 곱해준다.
+		if (exp % 2 == 1L) {
+			ret = multiply(ret, origin);
+		}
+
+		return ret;
+	}
+
+	// o1과 o2 행렬을 곱해주는 메소드
+	public static long[][] multiply(long[][] o1, long[][] o2) {
+
+		long[][] ret = new long[2][2];
+
+		ret[0][0] = ((o1[0][0] * o2[0][0]) + (o1[0][1] * o2[1][0])) % MOD;
+		ret[0][1] = ((o1[0][0] * o2[0][1]) + (o1[0][1] * o2[1][1])) % MOD;
+		ret[1][0] = ((o1[1][0] * o2[0][0]) + (o1[1][1] * o2[1][0])) % MOD;
+		ret[1][1] = ((o1[1][0] * o2[0][1]) + (o1[1][1] * o2[1][1])) % MOD;
+
+		return ret;
 	}
 
 }
--- 20221031/src/BOJ_6064.java
+++ 20221031/src/BOJ_6064.java
@@ -1,9 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/*
+ * 카잉 달력
+ */
 public class BOJ_6064 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 
+			int endday = lcm(M, N);
+			int result = -1; // 실패할 경우
+			
+			int i;
+			for (i = x; i <= endday; i += M) { // x는 M만큼 건너뜀
+				// i를 N으로 나눈 나머지가 y 라면 만족
+				// y == N 인 경우에는, i가 N으로 나눠떨어져야 만족
+				if((y == N && i % N == 0) || (i % N == y)) {
+					result = i;
+					break;
+				}
+			}
+			
+			System.out.println(result);
+		}
+
+	}
+
+	// 최대 공약수
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+
+	// 최소 공배수
+	public static int lcm(int a, int b) {
+		return a * b / gcd(a, b);
 	}
 
 }
