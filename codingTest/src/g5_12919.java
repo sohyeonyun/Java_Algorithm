7d3cd540179837d11d029dc7e77aff12a75caf7d
윤소현 <wja06018@naver.com>
2023-04-12 01:59:14 +0900

    20230412

--- codingTest/src/g5_12919.java
+++ codingTest/src/g5_12919.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 
 public class g5_12919 {
+	
+	static String start;
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		StringBuilder start = new StringBuilder(sc.nextLine());
+		start = sc.nextLine();
 		String end = sc.nextLine();
 
-		if (end.charAt(0) == 'A' && end.contains("B")) {
-			System.out.println(0);
-			return;
-		}
-
-		if (dfs(start, end)) {
+		if (dfs(end)) {
 			System.out.println(1);
 		} else {
 			System.out.println(0);
@@ -22,24 +19,30 @@ public class g5_12919 {
 
 	}
 	
-	static int num = 0;
-
-	private static boolean dfs(StringBuilder start, String end) {
-System.out.println(num++);
+	private static boolean dfs(String end) {
 		if (start.equals(end)) {
 			return true;
 		}
-
-		System.out.println(start + " " + end + " " + start.length() + " " + end.length());
-		System.out.println();
 		
-		if (start.length() >= end.length()) {
+		if(end.length() <= 1) {
 			return false;
 		}
 		
-		dfs(start.append("A"), end);
-		dfs(start.replace(start.length() - 1, start.length(), "B").reverse(), end);
-
+		// A 떼기
+		if(end.charAt(end.length() - 1) == 'A') {
+			if(dfs(end.substring(0, end.length()-1))) {
+				return true;
+			}
+		}
+		
+		// B 떼기
+		if(end.charAt(0) == 'B') {
+			StringBuilder sb = new StringBuilder(end);
+			if(dfs(sb.reverse().delete(sb.length() - 1, sb.length()).toString())) {
+				return true;
+			}
+		}
+		
 		return false;
 
 	}
