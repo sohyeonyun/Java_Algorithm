1d6b85b7bcaafe34ed0d70ff0f83d6fbbdb3ac8f
윤소현 <wja06018@naver.com>
2023-04-13 08:11:32 +0900

    20230413

--- codingTest/src/g5_1092.java
+++ codingTest/src/g5_1092.java
@@ -1,7 +1,5 @@
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class g5_1092 {
@@ -22,57 +20,45 @@ public class g5_1092 {
 			boxes[i] = sc.nextInt();
 		}
 
+		// 내림차순 정렬
 		Arrays.sort(craines, Collections.reverseOrder());
 		Arrays.sort(boxes, Collections.reverseOrder());
-		System.out.println(Arrays.toString(craines));
-		System.out.println(Arrays.toString(boxes));
 		
 		int answer = 1;
 		int craine_idx = 0;
 		int box_idx = 0;
 		while(box_idx < M) {
 			int box = boxes[box_idx];
-			System.out.println(box_idx + " " + box + "!!!!!!");
 			
 			// 크레인 하나씩 탐색
 			for(int j=0; j<M; j++) {
-				int idx = (craine_idx + j) % M;
-				System.out.println("박스: " + box_idx + " " + box + " 크레인: " + craine_idx + " " + craines[craine_idx]);
+				int cur = (craine_idx + j) % M;
+				
 				// 담기
-				if(craines[idx] >= box) {
+				if(craines[cur] >= box) {
 					// 마지막 크레인
 					if(craine_idx == N - 1) {
 						answer++;
 					}
 					// 다음 크레인
-					System.out.println(craine_idx);
 					craine_idx = (craine_idx + 1) % N;
-					System.out.println(craine_idx);
 					box_idx++;
 					break;
 				}
 				// 못 담아
 				else {
-					// 첫번째 크레인
+					// 첫번째 크레인인데 못담으면 다 못담음!!
 					if(craine_idx == 0) {
 						System.out.println(-1);
 						return;
 					}
-					// 마지막 크레인
-					if(craine_idx == N - 1) {
-						answer++;
-						box_idx = (box_idx + 1) % M;
-						craine_idx = 0;
-						break;
-					}
-					craine_idx++;
-					box_idx = (box_idx + 1) % M;
+					// 크레인 한번 옮기고 젤 앞 크레인으로 포인팅
+					answer++;
+					j = M - 1; // 마지막 회차로 시킴.
+					craine_idx = 0;
 				}
 				
 			}
-			
-			System.out.println(box_idx);
-			System.out.println();
 		}
 		
 		System.out.println(answer);
